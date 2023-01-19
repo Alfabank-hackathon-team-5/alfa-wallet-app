fa1ecc3c1f2d397b95cfb1454b66a6dafb106668
k1natazz <114512011+angelinabukinaa@users.noreply.github.com>
2023-01-19 05:43:47 +0300

    skeleton -

---
+++ main.py
@@ -0,0 +1,28 @@
+# Python 3.8 kivy 2.0.0
+
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang.builder import Builder
+
+class UserPage(Screen):
+    pass
+
+class LoginPage(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+class MainWindow(MDBoxLayout):
+    pass
+
+class WalletApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_file('walletapp.kv')
+
+
+if __name__ == "__main__":
+    WalletApp().run()
