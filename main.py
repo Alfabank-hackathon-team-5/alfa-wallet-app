b2dbceb9cf883a3c49c8b170932274055f796903
k1natazz <ruslan-alsherov@yandex.ru>
2023-01-20 04:01:15 +0300

    sc 3.1
    Исправлено:
    открытие нескольких qr окон одновременно
    переключение в режиме просмотра qr на другой экран

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class ElementCard(MDCard):
         app = MDApp.get_running_app()
         result_widget = app.root.ids.user_page.ids.screen_1
         background = app.root.ids.user_page
+        app.disable_cards(True)
         app.disable_navigation(2, 3, True)
         background.opacity = 0.1
         qr_page = QrPage()
@@ -19,6 +20,7 @@ class QrPage(MDCard):
     def hide_qr(self):
         app = MDApp.get_running_app()
         result_widget = app.root.ids.user_page.ids.screen_1
+        app.disable_cards(False)
         app.disable_navigation(2, 3, False)
         result_widget.remove_widget(self)
         background = app.root.ids.user_page
@@ -49,13 +51,33 @@ class WalletApp(MDApp):
     def disable_navigation(self, page1, page2, status):
         app = MDApp.get_running_app()
         if status == True:
-            if (page1, page2) == (2, 3):
+            if (page1, page2) == (1, 2):
+                app.root.ids.user_page.ids.navigation_bottom_1.disabled = True
+                app.root.ids.user_page.ids.navigation_bottom_2.disabled = True
+            elif (page1, page2) == (2, 3):
                 app.root.ids.user_page.ids.navigation_bottom_2.disabled = True
                 app.root.ids.user_page.ids.navigation_bottom_3.disabled = True
+            elif (page1, page2) == (1, 3):
+                app.root.ids.user_page.ids.navigation_bottom_1.disabled = True
+                app.root.ids.user_page.ids.navigation_bottom_3.disabled = True
         else:
-            if (page1, page2) == (2, 3):
+            if (page1, page2) == (1, 2):
+                app.root.ids.user_page.ids.navigation_bottom_1.disabled = False
+                app.root.ids.user_page.ids.navigation_bottom_2.disabled = False
+            elif (page1, page2) == (2, 3):
                 app.root.ids.user_page.ids.navigation_bottom_2.disabled = False
                 app.root.ids.user_page.ids.navigation_bottom_3.disabled = False
+            elif (page1, page2) == (1, 3):
+                app.root.ids.user_page.ids.navigation_bottom_1.disabled = False
+                app.root.ids.user_page.ids.navigation_bottom_3.disabled = False
+
+    def disable_cards(self, status):
+        app = MDApp.get_running_app()
+        if status == True:
+            app.root.ids.user_page.ids.card_list.disabled = True
+        else:
+            app.root.ids.user_page.ids.card_list.disabled = False
+
 
 if __name__ == "__main__":
     WalletApp().run()
