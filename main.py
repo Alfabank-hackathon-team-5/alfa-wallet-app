28379b173453bc970216a6156c7430cf048c5ff4
k1natazz <ruslan-alsherov@yandex.ru>
2023-01-20 03:51:21 +0300

    sk 3

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 # Python 3.8 kivy 2.0.0
-
+# Критически важно, чтобы название kv файла не совпадало с walletapp
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang.builder import Builder
+from kivymd.uix.card import MDCard
+
+class ElementCard(MDCard):
+    def show_qr(self):
+        app = MDApp.get_running_app()
+        result_widget = app.root.ids.user_page.ids.screen_1
+        background = app.root.ids.user_page
+        app.disable_navigation(2, 3, True)
+        background.opacity = 0.1
+        qr_page = QrPage()
+        result_widget.add_widget(qr_page)
+
+class QrPage(MDCard):
+    def hide_qr(self):
+        app = MDApp.get_running_app()
+        result_widget = app.root.ids.user_page.ids.screen_1
+        app.disable_navigation(2, 3, False)
+        result_widget.remove_widget(self)
+        background = app.root.ids.user_page
+        background.opacity = 1
+        
 
 class LoginPage(Screen):
     pass
@@ -14,15 +34,28 @@ class UserPage(Screen):
 class ScreenManagement(ScreenManager):
     pass
 
-class MainWindow(MDBoxLayout):
-    pass
-
 class WalletApp(MDApp):
+
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
-        return Builder.load_file('walletapp.kv')
+        return Builder.load_file('alfawalletapp.kv')
 
+    def add_card(self):
+        app = MDApp.get_running_app()
+        result_widget = app.root.ids.user_page.ids.card_list
+        result_widget.add_widget(ElementCard())
+
+    def disable_navigation(self, page1, page2, status):
+        app = MDApp.get_running_app()
+        if status == True:
+            if (page1, page2) == (2, 3):
+                app.root.ids.user_page.ids.navigation_bottom_2.disabled = True
+                app.root.ids.user_page.ids.navigation_bottom_3.disabled = True
+        else:
+            if (page1, page2) == (2, 3):
+                app.root.ids.user_page.ids.navigation_bottom_2.disabled = False
+                app.root.ids.user_page.ids.navigation_bottom_3.disabled = False
 
 if __name__ == "__main__":
     WalletApp().run()
